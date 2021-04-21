0c952d2d82db10a20bd6d0d5c8f95f4e8b82d946
deodatuss11 <80252544+deodatuss11@users.noreply.github.com>
2021-04-21 16:31:51 +0700

    Add files via upload

---
+++ mainQueue.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pertemuan7;
+
+/**
+ *
+ * @author Deodatuss
+ */
+public class mainQueue {
+
+   public static void main(String[] args) {
+   Queue.menuProgram();
+}
+}
